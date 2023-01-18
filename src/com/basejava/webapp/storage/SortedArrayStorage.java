5ae827a143056012055f2ffaa2e53f96041ce8cf
Евгения <ebogdanova@nobilis.team>
2023-01-18 22:58:31 +0100

    introduce abstract class AbstractArrayStorage and class SortedArrayStorage

---
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -0,0 +1,35 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void save(Resume resume) {
+
+    }
+
+    @Override
+    public void update(Resume resume) {
+
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        return null;
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+}
