c3483a5f7ef744c81cc318f465fe227da3ad49cc
Евгения <ebogdanova@nobilis.team>
2023-02-11 21:36:39 +0100

    ListStorage class implemented The operations of the ListStorage class is tested in the MainTestArrayStorage class

--- src/com/basejava/webapp/storage/ListStorage.java
+++ src/com/basejava/webapp/storage/ListStorage.java
@@ -1,5 +1,79 @@
 package com.basejava.webapp.storage;
 
+import com.basejava.webapp.exeption.ExistStorageException;
+import com.basejava.webapp.exeption.NotExistStorageException;
+import com.basejava.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListStorage extends AbstractStorage {
 
+    protected List<Resume> storage = new ArrayList<>();
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public void save(Resume resume) {
+        int index = findIndex(resume.getUuid());
+        if (index >= 0) {
+            throw new ExistStorageException(resume.getUuid());
+        } else {
+            storage.add(resume);
+        }
+    }
+
+    @Override
+    public void update(Resume resume) {
+        int index = findIndex(resume.getUuid());
+        if (index < 0) {
+            throw new NotExistStorageException(resume.getUuid());
+        } else {
+            storage.set(index, resume);
+        }
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        int index = findIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        } else {
+            return storage.get(index);
+        }
+    }
+
+    @Override
+    public void delete(String uuid) {
+        int index = findIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        } else {
+            storage.remove(index);
+        }
+    }
+
+    @Override
+    public Resume[] getAll() {
+        Resume[] results = new Resume[storage.size()];
+        results = storage.toArray(results);
+        return results;
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    protected int findIndex(String uuid) {
+        for (Resume resume : storage) {
+            if (uuid.equals(resume.getUuid())) {
+                return storage.indexOf(resume);
+            }
+        }
+        return -1;
+    }
 }
