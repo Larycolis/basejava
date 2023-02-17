2a06dbc5450c2f7e8d432a570c8105a4e130d301
Евгения <ebogdanova@nobilis.team>
2023-02-17 23:38:13 +0100

    Classes MapStorage and MapStorageTest implemented

--- src/com/basejava/webapp/storage/MapStorage.java
+++ src/com/basejava/webapp/storage/MapStorage.java
@@ -1,4 +1,63 @@
-//package com.basejava.webapp.storage;
+package com.basejava.webapp.storage;
 
-//public class MapStorage extends AbstractStorage {
-//}
+import com.basejava.webapp.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapStorage extends AbstractStorage {
+    protected final Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    protected void doSave(Resume resume, Object searchKey) {
+        storage.put((String) searchKey, resume);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, Object searchKey) {
+        storage.put((String) searchKey, resume);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return storage.get((String) searchKey);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        storage.remove((String) searchKey);
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.values().toArray(new Resume[0]);
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    // реализация метода на данный момент ошибочная
+    protected String getSearchKey(String uuid) {
+        Set<Map.Entry<String, Resume>> entries = storage.entrySet();
+        for (Map.Entry<String, Resume> pair : entries) {
+            String searchKey = pair.getKey();
+            if (uuid.equals(searchKey)) {
+                return searchKey;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return storage.containsKey((String) searchKey);
+    }
+}
