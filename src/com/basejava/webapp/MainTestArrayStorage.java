9e660ac59ccbdb547890dd2ba352d8ee960dcee5
Евгения <ebogdanova@nobilis.team>
2023-03-01 12:05:25 +0100

    minor changes in the test class: additional testing

--- src/com/basejava/webapp/MainTestArrayStorage.java
+++ src/com/basejava/webapp/MainTestArrayStorage.java
@@ -11,15 +11,15 @@ public class MainTestArrayStorage {
     private final static Storage ARRAY_STORAGE = new MapUuidStorage(); // new ListStorage(); // new ArrayStorage(); // new SortedArrayStorage();
 
     public static void main(String[] args) {
-        final Resume r1 = new Resume("uuid1");
-        final Resume r2 = new Resume("uuid2");
-        final Resume r3 = new Resume("uuid3");
+        final Resume r1 = new Resume("uuid1", "Иванов Иван Иванович");
+        final Resume r2 = new Resume("uuid2", "Петров Петр Петрович");
+        final Resume r3 = new Resume("uuid3", "Сидоров Сидор Сидорович");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r3);
         ARRAY_STORAGE.save(r2);
 
-        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.printf("Get r1 \n uuid: %s \n fullName: %s \n", ARRAY_STORAGE.get(r1.getUuid()), r1.getFullName());
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
