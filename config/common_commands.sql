e2686a575bcd5484c3d5ef4b0af6c64e8fbc505e
Евгения <ebogdanova@nobilis.team>
2023-05-12 12:12:30 +0200

    implemented method getAllSorted in accordance with the condition from the HW

--- config/common_commands.sql
+++ config/common_commands.sql
@@ -13,7 +13,13 @@ SELECT *
 FROM resume r
          LEFT JOIN contact c
                    ON r.uuid = c.resume_uuid
-WHERE r.uuid='uuid';
+WHERE r.uuid='92862cdc-2584-4cee-b3da-1c6bfd7d36a1';
+
+SELECT *
+FROM resume r
+         LEFT JOIN section s
+                   ON r.uuid = s.resume_uuid
+WHERE r.uuid='92862cdc-2584-4cee-b3da-1c6bfd7d36a1';
 
 DELETE
 FROM resume
--- src/com/basejava/webapp/storage/SQLStorage.java
+++ src/com/basejava/webapp/storage/SQLStorage.java
@@ -16,6 +16,7 @@ if (!rs.next()) - если в результате отправки команд
 Когда в типизированном методе ничего не нужно возвращать нужно пометить это конструкцией, например, sqlHelper.<Void>.execute()
  */
 
+// DONE сделать реализацию getAllSorted через два отдельных запроса: резюме, контакты, секции (except OrganizationSection)
 // TODO implement Section (except OrganizationSection)
 // TODO Join and split ListSection by '\n'
 public class SQLStorage implements Storage {
@@ -103,25 +104,39 @@ public class SQLStorage implements Storage {
     @Override
     public List<Resume> getAllSorted() {
         LOG.info("getAllSorted");
-        return sqlHelper.execute("" +
-                        "SELECT * FROM resume r " +
-                        "LEFT JOIN contact c " +
-                        "ON r.uuid = c.resume_uuid " +
-                        "ORDER BY r.full_name, r.uuid",
-                ps -> {
-                    ResultSet rs = ps.executeQuery();
-                    Map<String, Resume> map = new LinkedHashMap<>();
-                    while (rs.next()) {
-                        String uuid = rs.getString("uuid");
-                        Resume resume = map.get(uuid);
-                        if (resume == null) {
-                            resume = new Resume(uuid, rs.getString("full_name"));
-                            map.put(uuid, resume);
-                        }
-                        doAddContact(resume, rs);
+        return sqlHelper.transactionExecute(conn -> {
+            Map<String, Resume> map = new LinkedHashMap<>();
+
+            try (PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume r ORDER BY r.full_name, r.uuid")) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    String uuid = rs.getString("uuid");
+                    Resume resume = map.get(uuid);
+                    if (resume == null) {
+                        resume = new Resume(uuid, rs.getString("full_name"));
+                        map.put(uuid, resume);
                     }
-                    return new ArrayList<>(map.values());
-                });
+                }
+            }
+
+            try (PreparedStatement ps = conn.prepareStatement("SELECT * FROM contact c")) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    Resume resume = map.get(rs.getString("resume_uuid"));
+                    doAddContact(resume, rs);
+                }
+            }
+
+            try (PreparedStatement ps = conn.prepareStatement("SELECT * FROM section s")) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    Resume resume = map.get(rs.getString("resume_uuid"));
+                    doAddSection(resume, rs);
+                }
+            }
+
+            return new ArrayList<>(map.values());
+        });
     }
 
     @Override
@@ -145,10 +160,20 @@ public class SQLStorage implements Storage {
         }
     }
 
+    private void doInsertSections(Connection conn, Resume resume) throws SQLException {
+    }
+
     private void doAddContact(Resume resume, ResultSet rs) throws SQLException {
         final String value = rs.getString("value");
         if (value != null) {
             resume.addContact(ContactType.valueOf(rs.getString("type")), value);
+        }
+    }
+
+    private void doAddSection(Resume resume, ResultSet rs) throws SQLException {
+        String value = rs.getString("value");
+        if (value != null) {
+            resume.addContact(ContactType.valueOf(rs.getString("type")), value); // вместо value тут нужно что-то типа свич кейса для разных видов секций
         }
     }
 }
