a167fe88bc1fb390ecd6b4c78114995ac85b0699
Евгения <ebogdanova@nobilis.team>
2023-05-12 11:17:05 +0200

    table section created in DB

--- config/common_commands.sql
+++ config/common_commands.sql
@@ -13,8 +13,8 @@ SELECT *
 FROM resume r
          LEFT JOIN contact c
                    ON r.uuid = c.resume_uuid
-WHERE r.uuid='6fd43d30-de89-4353-9290-fc19ec7f7ce7';
+WHERE r.uuid='uuid';
 
 DELETE
 FROM resume
-WHERE uuid='6fd43d30-de89-4353-9290-fc19ec7f7ce7';
+WHERE uuid='uuid';
