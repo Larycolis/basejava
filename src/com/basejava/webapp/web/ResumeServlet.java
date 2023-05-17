396885fce9341f721b6b10ceae75a7af7af89644
Евгения <ebogdanova@nobilis.team>
2023-05-17 22:39:43 +0200

    an unsuccessful attempt to write a ResumeServlet

--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -1,24 +1,67 @@
 package com.basejava.webapp.web;
 
+import com.basejava.webapp.Config;
+import com.basejava.webapp.model.Resume;
+import com.basejava.webapp.storage.Storage;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.getConfig().getStorage();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         //response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+//        String name = request.getParameter("name");
+//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+
+        Writer writer = response.getWriter();
+        writer.write("""
+                 <html>
+                 <body>
+                 <h2>Resumes Table</h2>
+                 <table>
+                 <thead>
+                 <tr>
+                 <th>Uuid</th>
+                 <th>Full name</th>
+                 </tr>
+                 </thead>
+                """);
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write("""
+                    <tr>
+                    <th>resume.getUuid()</th>
+                    <th>resume.getFullName()</th>
+                    </tr>
+                    """);
+        }
+        writer.write("""
+                </table>
+                </body>
+                </html>
+                """);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
+
+
 }
